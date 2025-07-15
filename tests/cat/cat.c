bf8371c3437f0d0633b96dd81aa58fb3edf2e629
Billy <141384133+PaulusBilly@users.noreply.github.com>
2025-07-16 00:03:27 +0700

    tests comparison and loops

---
+++ tests/cat/cat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  // FOR LOOP
+  for (int i = 0; i < 3; i++)
+  {
+    printf("meow...\n");
+  }
+
+  // WHILE LOOP
+  int i = 0;
+  while (i < 3)
+  {
+    printf("meow!\n");
+    i++;
+  }
+}
