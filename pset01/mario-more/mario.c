20f93d29c25717502bcb27c159ead1c9f959e963
Billy <141384133+PaulusBilly@users.noreply.github.com>
2025-07-24 15:58:25 +0700

    mario-more progress

---
+++ pset01/mario-more/mario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main() {
+  int height;
+  int length;
+  do {
+    height = get_int("Height: ");
+    length = height;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < length; j++) {
+        printf("#");
+      }
+      printf(" ")
+    }
+  } while (!(height < 0))
+}
