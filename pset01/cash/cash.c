847fffdfb2700aade11909029546f7d360b41412
PaulusBilly <paulus.billiardo@gmail.com>
2025-07-25 13:25:45 +0700

    [pset01]: completed cash

--- pset01/cash/cash.c
+++ pset01/cash/cash.c
@@ -2,29 +2,33 @@
 #include <cs50.c>
 
 int main() {
-    int counter = 0;
     int owed;
+    int counter = 0;
 
     do {
         owed = get_int("Change owed: ");
-    } while (owed <= 0);
+    } while (owed < 0);
 
-    while (owed == 0) {
-        if (owed >= 25) {
-            counter++;
-            owed -= 25;
-        } else if (owed >= 10 && owed < 25) {
-            counter++;
-            owed -= 10;
-        } else if (owed>= 5 && owed< 10) {
-            counter++;
-            owed -= 5;
-        } else if (owed >= 1 && owed < 5) {
-            counter ++;
-            owed -= 1;
-        }
+    while (owed >= 25) {
+        owed -= 25;
+        counter++;
     }
 
-    printf("Counter = %i", counter);
+    while (owed >= 10) {
+        owed -= 10;
+        counter++;
+    }
+
+    while (owed >= 5) {
+        owed -= 5;
+        counter++;
+    }
+
+    while (owed >= 1) {
+        owed -= 1;
+        counter++;
+    }
+
+    printf("%i\n", counter);
 }
 
