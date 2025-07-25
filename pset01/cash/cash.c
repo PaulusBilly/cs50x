8bf2fd51d540e80ab4cf6debd9acbe0628066a5f
PaulusBilly <paulus.billiardo@gmail.com>
2025-07-25 13:02:53 +0700

    still confused

--- pset01/cash/cash.c
+++ pset01/cash/cash.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <cs50.c>
 
 int main() {
-  int counter = 0;
-  int owed;
-  
-  while (owed >= 0) {
-    owed = get_int("Change owed: ");
-    if (owed > 25) {
-      owed -= 25;
-      counter++;
-    } else if (25 > owed <= 10) {
-      owed -= 10;
-      counter++;
-    } else if (10 > owed <= 5) {
-      owed -= 5;
-      counter++;
-    } else {
-      owed -= 1;
-      counter++;
+    int counter = 0;
+    int owed;
+
+    do {
+        owed = get_int("Change owed: ");
+    } while (owed <= 0);
+
+    while (owed == 0) {
+        if (owed >= 25) {
+            counter++;
+            owed -= 25;
+        } else if (owed >= 10 && owed < 25) {
+            counter++;
+            owed -= 10;
+        } else if (owed>= 5 && owed< 10) {
+            counter++;
+            owed -= 5;
+        } else if (owed >= 1 && owed < 5) {
+            counter ++;
+            owed -= 1;
+        }
     }
 
-    printf("%i\n", counter);
-  }
+    printf("Counter = %i", counter);
 }
 
